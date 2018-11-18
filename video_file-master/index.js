1c19af5c75e370b0158f7d86d0fcbd322411365f
Shyard <45152338+Shyard@users.noreply.github.com>
2018-11-18 23:35:56 +0100

    Update index.js

--- video_file-master/index.js
+++ video_file-master/index.js
@@ -1,8 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const cfg require('./index.json'); 
-const token = process.env.token; 
-const prefix = ("?");
+const prefix = (cfg.prefix);
 
 bot.on('ready', function () {
     console.log("Je suis apte à être utilisé.")
