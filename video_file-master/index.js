5b4b243552cdc146ce0ae8c73297829ffd62f02e
Shyard <45152338+Shyard@users.noreply.github.com>
2018-11-18 23:36:53 +0100

    Update index.js

--- video_file-master/index.js
+++ video_file-master/index.js
@@ -1,13 +1,11 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const cfg require('./index.json'); 
+const token = process.env.token; 
 const prefix = (cfg.prefix);
 
 bot.on('ready', function () {
     console.log("Je suis apte à être utilisé.")
     bot.user.setActivity('Chasser la femelle').catch(console.error)
 });
-
-
-bot.login(token);
 
